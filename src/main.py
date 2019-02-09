b02ff16261703c18ef5f7cd263efa4df437dc1aa
Daniel Qiang <daniel_qiang@hotmail.com>
2019-02-09 13:03:13 -0800

    added comments

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
-from src import Poll
+from src import Bot
 
 
 def main():
-    user = 'MyUW Username (@uw.edu email)'
-    password = 'MyUW Password'
+    user = 'My Username'
+    password = 'My Password'
     host = 'PollEverywhere URL Extension e.g. "uwpsych"'
 
-    with Poll(user, password, host) as poll:
-        poll.run()
+    # If you're not using a UW PollEv account, just omit the 'organization' argument
+    with Bot(user, password, host, organization='uw') as bot:
+        bot.run()
 
 
 if __name__ == '__main__':
