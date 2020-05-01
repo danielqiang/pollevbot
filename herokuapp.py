4f5938b9007ce52740ad7b7607ebe3d76fa8b167
Daniel Qiang <daniel_qiang@hotmail.com>
2020-05-01 02:54:53 -0700

    Add heroku files

---
+++ herokuapp.py
@@ -0,0 +1,32 @@
+import os
+import logging
+from datetime import date
+from pollevbot import PollBot
+
+logger = logging.getLogger(__name__)
+
+
+def check_day():
+    # DAY_OF_WEEK is a string of ints delimited
+    # by commas, i.e. day_of_week = '1,3' means
+    # run on tuesday and thursday
+    day_of_week = [s.strip() for s in os.environ['DAY_OF_WEEK'].split(',')]
+
+    return str(date.today().weekday()) in day_of_week
+
+
+def main():
+    # Heroku config vars
+    user = os.environ['USERNAME']
+    password = os.environ['PASSWORD']
+    host = os.environ['POLLHOST']
+
+    if check_day():
+        with PollBot(user, password, host) as bot:
+            bot.run()
+    else:
+        logger.info("pollevbot is not configured to run today. Exiting.")
+
+
+if __name__ == '__main__':
+    main()
